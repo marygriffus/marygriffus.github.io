bb6e69c939e8a04ffd03bcc340898ce01370c9e0
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-24 14:01:13 -0400

    changed styling, added projects

---
+++ app.js
@@ -0,0 +1,14 @@
+document.querySelector(".block.image.one").addEventListener("mouseover", function(){
+  console.log('here');
+  document.querySelector(".project-info").innerHTML="We made an app to help people find a perfect car for their price point on Ebay. I love the beautiful graphs it can generate and that it can pull from a real data set to help solve a problem. I put together the API calls and connected them with the graph, then plotted the line of best fit.";
+})
+
+document.querySelector(".block.image.two").addEventListener("mouseover", function(){
+  console.log('here');
+  document.querySelector(".project-info").innerHTML="I created this site to help women find their best-fitting bras through precise metrics.";
+})
+
+document.querySelector(".block.image.three").addEventListener("mouseover", function(){
+  console.log('here');
+  document.querySelector(".project-info").innerHTML="Concentration: change the difficulty level, try for a better time, or play... in space!";
+})
