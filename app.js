1fe5f415b7127b5742951409a086551edc2500e6
Mary Griffus <mary.e.shultz@gmail.com>
2016-06-07 09:47:19 -0400

    removed logs, changed text color, add text for trains

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 document.querySelector(".block.image.one").addEventListener("mouseover", function(){
-  console.log('here');
-  document.querySelector(".project-info").innerHTML="Autograph: We made an app to help people find a perfect car for their price point on Ebay. I love the beautiful graphs it can generate and that it can pull from a real data set to help solve a problem. I put together the API calls and connected them with the graph, then plotted the line of best fit.";
+  document.querySelector(".project-info").innerHTML="DC Train Tracker: This is a simple site to see the next trains and relevant alerts for a given Metro station. Available on mobile.";
 })
 
 document.querySelector(".block.image.two").addEventListener("mouseover", function(){
-  console.log('here');
-  document.querySelector(".project-info").innerHTML="Cups of Data: I created this site to help women find their best-fitting bras through precise metrics.";
+  document.querySelector(".project-info").innerHTML="Autograph: We made an app to help people find a perfect car for their price point on Ebay. I love the beautiful graphs it can generate and that it can pull from a real data set to help solve a problem. I put together the API calls and connected them with the graph, then plotted the line of best fit.";
 })
 
 document.querySelector(".block.image.three").addEventListener("mouseover", function(){
-  console.log('here');
+  document.querySelector(".project-info").innerHTML="Cups of Data: I created this site to help women find their best-fitting bras through precise metrics.";
+})
+
+document.querySelector(".block.image.four").addEventListener("mouseover", function(){
   document.querySelector(".project-info").innerHTML="Concentration: change the difficulty level, try for a better time, or play... in space!";
 })
